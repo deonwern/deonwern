5ba403201e23160146c6ff4bfabef4d4ffa7b456
Deon wern <mail@deonwern.uk>
2026-09-12 18:25:31 +0530

    [v0.0.9] delete(components): deleted journey component.
    deleted other empty file, can be added in future...

--- src/lib/components/oath.svelte
+++
@@ -1,7 +0,0 @@
-<section>
-	<p>
-		Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt tempore impedit eaque enim
-		odit repudiandae in? Vel officiis enim nesciunt, nam quae ex est consequuntur iusto veritatis
-		similique nihil ipsa?
-	</p>
-</section>
