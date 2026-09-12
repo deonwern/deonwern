a92bd1114c567010458d52fe70e5977eb342585d
Deon wern <mail@deonwern.uk>
2026-09-12 18:58:53 +0530

    [v0.0.14] fix(hero): increased size of text.
    makes it look bolder...

--- src/lib/components/hero.svelte
+++ src/lib/components/hero.svelte
@@ -1,5 +1,5 @@
 <section class="mx-auto max-w-3xl px-4 py-20 text-left">
-	<h1 class="mb-6 text-5xl font-bold tracking-tight md:text-7xl">
+	<h1 class="mb-6 text-6xl font-bold tracking-tight md:text-8xl">
 		Independent open-core software developer.
 	</h1>
 	<p class="text-xl italic">Sharing the open-core build journey on YouTube.</p>
